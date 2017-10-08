582ae4b74f699c81c2bc7d9d7302f05799da5164
chengyuen <cedric_ong93@hotmail.com>
2017-10-08 14:18:21 +0800

    added new formula to calculate distance and trajectory

--- scripts/robot_correction.py
+++ scripts/robot_correction.py
@@ -17,6 +17,7 @@ max_correction_run 			= 0.0
 balance_left_right          = 0.0
 
 need_correction				= False
+test_new_formula			= True
 
 from math import radians, cos, sin, asin, sqrt, atan2, degrees
 
@@ -29,6 +30,22 @@ from math import radians, cos, sin, asin, sqrt, atan2, degrees
 #aaron : 1/6/2017, this was added because we cant accurately tune how much the robot deviates while going straight
 #so this solution is only suitable for demo, while direction is straight, no deviation is assumed
 #while turning, no translation is assumed
+
+def get_dist_angle(left_encode, right_encode):
+	t = 0.1
+	vl = 0.114139/500.0 * left_encode
+	vr = 0.114139/500.0 * right_encode
+
+	vlr_sqrt = math.sqrt((vl - vr)**2 + (2*robot_drive.turn_radius)**2)
+
+	v = (2*robot_drive.turn_radius*(vl+vr))/float(2*vlr_sqrt)
+
+	dist = v * t * 1000 #mm
+
+	theta = math.acos((2*v)/float(vl+vr))
+	theta = math.degrees(theta) #degree
+	return dist, theta
+
 def update_robot_gps_new(left_encode, right_encode):
 	if(left_encode == 0 and right_encode == 0):
 		#no updating of information
@@ -66,92 +83,110 @@ def update_robot_gps_new(left_encode, right_encode):
 		rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
 
 def update_robot_gps(left_encode, right_encode):
+	global test_new_formula
+
 	robot_drive.step_angle = 0.0
 	robot_drive.step_distance = 0.0
 
-	#scenario 1, robot not moving
-	if(left_encode == 0 and right_encode == 0):
-		#no updating of information
-				#@yuqing_continueturn
-		# robot_publisher.publish_gps()
-		return
+	if test_new_formula:
+		if (left_encode == 0 and right_encode == 0):
+			return
 
-	# loacal vaiables
+		robot_drive.step_distance, robot_drive.step_angle = get_dist_angle(left_encode, right_encode)
+		if left_encode < right_encode:
+			robot_drive.step_angle = - robot_drive.step_angle
+		bearing 				= robot_drive.bearing_now + robot_drive.step_angle
+		bearing 				= gpsmath.format_bearing(bearing)
 
-	if (robot_drive.direction == "forward" or robot_drive.direction == "backward"):
-		left_dist 	= float(left_encode) / robot_drive.linear_encode_to_mm
-		left_dist   = left_dist / balance_left_right
-		right_dist 	= float(right_encode) / robot_drive.linear_encode_to_mm
-		# left_dist_turn  = float(left_encode) / robot_drive.turning_encode_to_mm 	#aaron 8 july
-		# right_dist_turn = float(right_encode) / robot_drive.turning_encode_to_mm 	#aaron 8 july
-	else :
-		left_dist 	= float(left_encode) / robot_drive.turning_encode_to_mm
-		left_dist   = left_dist / balance_left_right
-		right_dist 	= float(right_encode) / robot_drive.turning_encode_to_mm
-		# left_dist_turn  = float(left_encode) / robot_drive.turning_encode_to_mm 	#aaron 8 july
-		# right_dist_turn = float(right_encode) / robot_drive.turning_encode_to_mm	#aaron 8 july
+		if robot_drive.show_log:
+			rospy.loginfo("Step_angle %f degree, Step_distance calculated %f mm", robot_drive.step_angle, robot_drive.step_distance)
+		robot_drive.lon_now, robot_drive.lat_now 	= gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now, robot_drive.step_distance, bearing)
+		robot_drive.bearing_now 			= bearing
 
-	alpha 		= 0.0 # step turn angle in radian
-	total_dist 	= abs(left_dist) + abs(right_dist)
-	R 			= 0.0 # turn raidus, if two wheels in same direction, then the faster wheel radius is R + robot_drive.turn_radius
-	# the slower wheel is R - robot_drive.turn_radius
-	# total_dist_turn = abs(left_dist_turn) + abs(right_dist_turn)
+	elif not test_new_formula:
+		#scenario 1, robot not moving
+		if(left_encode == 0 and right_encode == 0):
+			#no updating of information
+					#@yuqing_continueturn
+			# robot_publisher.publish_gps()
+			return
 
-	# global vaiables
-	robot_drive.step_angle 		= 0.0
-	robot_drive.step_distance 	= (left_dist + right_dist) / 2.0
+		# loacal vaiables
 
-	#rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
-	# scenario 01, 02 robot moving perfectly straight, bearing won't change, while lan and lon need to be updated
-	if(left_dist == right_dist):
-		# if(right_dist > 0.0 ):
-		# 	robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now , right_dist, robot_drive.bearing_now)
-		# if(right_dist < 0.0):
-		# 	robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now , right_dist, -robot_drive.bearing_now)
-		robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now , right_dist, robot_drive.bearing_now) 		#chengyuen16/7
+		if (robot_drive.direction == "forward" or robot_drive.direction == "backward"):
+			left_dist 	= float(left_encode) / robot_drive.linear_encode_to_mm
+			left_dist   = left_dist / balance_left_right
+			right_dist 	= float(right_encode) / robot_drive.linear_encode_to_mm
+			# left_dist_turn  = float(left_encode) / robot_drive.turning_encode_to_mm 	#aaron 8 july
+			# right_dist_turn = float(right_encode) / robot_drive.turning_encode_to_mm 	#aaron 8 july
+		else :
+			left_dist 	= float(left_encode) / robot_drive.turning_encode_to_mm
+			left_dist   = left_dist / balance_left_right
+			right_dist 	= float(right_encode) / robot_drive.turning_encode_to_mm
+			# left_dist_turn  = float(left_encode) / robot_drive.turning_encode_to_mm 	#aaron 8 july
+			# right_dist_turn = float(right_encode) / robot_drive.turning_encode_to_mm	#aaron 8 july
+
+		alpha 		= 0.0 # step turn angle in radian
+		total_dist 	= abs(left_dist) + abs(right_dist)
+		R 			= 0.0 # turn raidus, if two wheels in same direction, then the faster wheel radius is R + robot_drive.turn_radius
+		# the slower wheel is R - robot_drive.turn_radius
+		# total_dist_turn = abs(left_dist_turn) + abs(right_dist_turn)
+
+		# global vaiables
+		robot_drive.step_angle 		= 0.0
+		robot_drive.step_distance 	= (left_dist + right_dist) / 2.0
+
 		#rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
-		robot_publisher.publish_gps()
-		return
-	# scenario 02 robot moving forward with slight
-	# robot not so perfectly walking forward, eigher left wheel is faster or right wheel is faster
-	elif(left_dist > 0.0 and right_dist > 0.0):
-		# a little bit of right turning
-		alpha 	= (left_dist - right_dist) / (2.0 * robot_drive.turn_radius)
-		R 	= (total_dist * robot_drive.turn_radius) / abs(left_dist - right_dist) 		#aaron 8 july
+		# scenario 01, 02 robot moving perfectly straight, bearing won't change, while lan and lon need to be updated
+		if(left_dist == right_dist):
+			# if(right_dist > 0.0 ):
+			# 	robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now , right_dist, robot_drive.bearing_now)
+			# if(right_dist < 0.0):
+			# 	robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now , right_dist, -robot_drive.bearing_now)
+			robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now , right_dist, robot_drive.bearing_now) 		#chengyuen16/7
+			#rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
+			robot_publisher.publish_gps()
+			return
+		# scenario 02 robot moving forward with slight
+		# robot not so perfectly walking forward, eigher left wheel is faster or right wheel is faster
+		elif(left_dist > 0.0 and right_dist > 0.0):
+			# a little bit of right turning
+			alpha 	= (left_dist - right_dist) / (2.0 * robot_drive.turn_radius)
+			R 	= (total_dist * robot_drive.turn_radius) / abs(left_dist - right_dist) 		#aaron 8 july
 
-	# scenario 04 robot moving backward
-	elif(left_dist < 0.0 and right_dist < 0.0):
-		alpha 	= (left_dist - right_dist) / (2.0 * robot_drive.turn_radius)
-		R 	= -total_dist * robot_drive.turn_radius / abs(right_dist - left_dist) 		#aaron 8 july
+		# scenario 04 robot moving backward
+		elif(left_dist < 0.0 and right_dist < 0.0):
+			alpha 	= (left_dist - right_dist) / (2.0 * robot_drive.turn_radius)
+			R 	= -total_dist * robot_drive.turn_radius / abs(right_dist - left_dist) 		#aaron 8 july
 
-	# for robot two wheels not moving at the same direction or once of the thing not moving
-	# forwaring with rotation
-	else:
-		alpha 	= total_dist / (2.0 * robot_drive.turn_radius)
-		r1 		= abs(left_dist) / alpha
-		r2 		= abs(right_dist) / alpha
-		R 		= abs(r1 - r2)
-
-		#right turn
-		if(left_dist >= 0.0 and right_dist < 0.0) or (right_dist == 0 and left_dist >0):
-		#if(left_dist > 0.0 or (left_dist == 0.0 and right_dist < 0.0)):
-			alpha = alpha
+		# for robot two wheels not moving at the same direction or once of the thing not moving
+		# forwaring with rotation
 		else:
-			alpha = -alpha
-	#rospy.logerr(R)
-	robot_drive.step_angle 	= degrees(alpha)
-	# covnert to degree
-	#####bearing 				= robot_drive.bearing_now + robot_drive.step_angle / 2.0
-	bearing = robot_drive.bearing_now + robot_drive.step_angle
-	# convert between [0 - 360)
-	bearing 				= gpsmath.format_bearing(bearing)
-	dist 					= R * sin(abs(alpha/2.0)) * 2.0
-	if robot_drive.show_log:
-		rospy.loginfo("Step in straight line %f mm, Step_angle %f degree, R %f mm, Step_distance calculated %f mm", dist, robot_drive.step_angle, R, robot_drive.step_distance)
-	robot_drive.lon_now, robot_drive.lat_now 	= gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now, dist, bearing)
-	robot_drive.bearing_now 			= bearing
-	robot_publisher.publish_gps()
-	#rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
+			alpha 	= total_dist / (2.0 * robot_drive.turn_radius)
+			r1 		= abs(left_dist) / alpha
+			r2 		= abs(right_dist) / alpha
+			R 		= abs(r1 - r2)
+
+			#right turn
+			if(left_dist >= 0.0 and right_dist < 0.0) or (right_dist == 0 and left_dist >0):
+			#if(left_dist > 0.0 or (left_dist == 0.0 and right_dist < 0.0)):
+				alpha = alpha
+			else:
+				alpha = -alpha
+		#rospy.logerr(R)
+		robot_drive.step_angle 	= degrees(alpha)
+		# covnert to degree
+		#####bearing 				= robot_drive.bearing_now + robot_drive.step_angle / 2.0
+		bearing = robot_drive.bearing_now + robot_drive.step_angle
+		# convert between [0 - 360)
+		bearing 				= gpsmath.format_bearing(bearing)
+		dist 					= R * sin(abs(alpha/2.0)) * 2.0
+		if robot_drive.show_log:
+			rospy.loginfo("Step in straight line %f mm, Step_angle %f degree, R %f mm, Step_distance calculated %f mm", dist, robot_drive.step_angle, R, robot_drive.step_distance)
+		robot_drive.lon_now, robot_drive.lat_now 	= gpsmath.get_gps(robot_drive.lon_now, robot_drive.lat_now, dist, bearing)
+		robot_drive.bearing_now 			= bearing
+		robot_publisher.publish_gps()
+		#rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
 
 def dist_correction_normal():
 	rospy.loginfo("************** Check errors after a normal job **************")
